ee02cd1ad5069ac6e6c09b083aeda3f59fabcfc1
Mathis Gauthey <mathis.gauthey@gmail.com>
2023-10-18 13:26:38 +0200

    exo1

--- 3AS5/TP1/exo1.c
+++ 3AS5/TP1/exo1.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
-int main (int argc, char*argv[])
+int main(int argc, char *argv[])
 {
     printf("Bonjour \n");
     return 0;
