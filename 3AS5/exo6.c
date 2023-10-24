82bb8e3b5cc7d9d16ebe79356990e8ea395f67bf
Mathis Gauthey <mathis.gauthey@gmail.com>
2023-10-24 16:58:06 +0200

    somme_positifis

--- 3AS5/exo6.c
+++ 3AS5/exo6.c
@@ -3,15 +3,52 @@
 #include <math.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
+int somme_entiers(int N)
 {
-    int N, compteur = 0;
-    printf("Veuillez choisir un nombre N: \n");
-    scanf("%d", &N);
+    int compteur = 0;
     for (int i = 1; i <= N; i++)
     {
         compteur += i;
     }
-    printf("La somme des %d premiers entiers est %d.\n", N, compteur);
+    return compteur;
+}
+
+int somme_entiers_pair(int N)
+{
+    int compteur = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        if (i % 2 == 0)
+        {
+            compteur += i;
+        }
+    }
+    return compteur;
+}
+
+int somme_entiers_impair(int N)
+{
+    int compteur = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        if (i % 2 != 0)
+        {
+            compteur += i;
+        }
+    }
+    return compteur;
+}
+
+int main(int argc, char *argv[])
+{
+    int N, somme_entier = 0, somme_entier_pair = 0, somme_entier_impair = 0, compteur;
+    printf("Veuillez choisir un nombre N: \n");
+    scanf("%d", &N);
+    somme_entier = somme_entiers(N);
+    somme_entier_pair = somme_entiers_pair(N);
+    somme_entier_impair = somme_entiers_impair(N);
+    printf("La somme des %d premiers entiers est %d.\n", N, somme_entier);
+    printf("La somme des %d premiers entiers pairs est %d.\n", N, somme_entier_pair);
+    printf("La somme des %d premiers entiers impairs est %d.\n", N, somme_entier_impair);
     return 0;
 }
