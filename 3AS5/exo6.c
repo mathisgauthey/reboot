d853db7a722d940b8af7f3632af86cfa51189459
Mathis Gauthey <mathis.gauthey@gmail.com>
2023-10-24 17:33:15 +0200

    fibonacci

--- 3AS5/exo6.c
+++ 3AS5/exo6.c
@@ -39,7 +39,19 @@ int somme_entiers_impair(int N)
     return compteur;
 }
 
-int main(int argc, char *argv[])
+int fibonacci(int k, int P0, int P1)
+{
+    int pn = 0, pn_1 = P1, pn_2 = P0;
+    printf("%d\n%d\n", pn_2, pn_1);
+    for (int i = 2; i <= k; i++)
+    {
+        pn = pn_1 + pn_2;
+        printf("%d\n", pn);
+        pn_2 = pn_1;
+        pn_1 = pn;
+    }
+}
+main(int argc, char *argv[])
 {
     int N, somme_entier = 0, somme_entier_pair = 0, somme_entier_impair = 0, compteur;
     printf("Veuillez choisir un nombre N: \n");
@@ -50,5 +62,11 @@ int main(int argc, char *argv[])
     printf("La somme des %d premiers entiers est %d.\n", N, somme_entier);
     printf("La somme des %d premiers entiers pairs est %d.\n", N, somme_entier_pair);
     printf("La somme des %d premiers entiers impairs est %d.\n", N, somme_entier_impair);
+
+    int k = 0, fibo = 0, P0 = 0, P1 = 1;
+    printf("Veuillez choisir un nombre k: \n");
+    scanf("%d", &k);
+    printf("Les %d premiers termes de la suite de Fibonacci sont: \n", k);
+    fibonacci(k, P0, P1);
     return 0;
 }
