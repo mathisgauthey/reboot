95c8a7b7fe8ee4b4a9e1d456f26f485512ec8351
Mathis Gauthey <mathis.gauthey@gmail.com>
2023-10-30 17:16:34 +0100

    TP2 terminé

--- 3AS5/TP2/Correction_Exo3.c
+++ 3AS5/TP2/Correction_Exo3.c
@@ -5,71 +5,70 @@
 double fct(double x)
 {
     double res;
-    res=x-cos(x);
+    res = x - cos(x);
     return res;
 }
 
-void pointfixe(double x0,double eps)
+void pointfixe(double x0, double eps)
 {
-    double xn_1,xn=x0,erreur;
-    int compteur=0;
+    double xn_1, xn = x0, erreur;
+    int compteur = 0;
     do
     {
-        xn_1=cos(xn);
-        erreur=fabs(xn_1-xn);
-        xn=xn_1;
-        compteur+=1;
-    }while(erreur>eps);
-    printf("La racine de f par la methode des points fixes est %.20lf \nLa methode a necessité %d\n",xn_1,compteur);
+        xn_1 = cos(xn);
+        erreur = fabs(xn_1 - xn);
+        xn = xn_1;
+        compteur += 1;
+    } while (erreur > eps);
+    printf("La racine de f par la methode des points fixes est %.20lf \nLa methode a necessitï¿½ %d\n", xn_1, compteur);
 }
 
-void newton(double x0,double eps)
+void newton(double x0, double eps)
 {
-    double xn_1,xn=x0,erreur;
-    int compteur=0;
+    double xn_1, xn = x0, erreur;
+    int compteur = 0;
     do
     {
-        xn_1=xn - ( (xn-cos(xn)) / (1+sin(xn)) );
-        erreur=fabs((xn_1-xn)/xn_1);
-        xn=xn_1;
-        compteur+=1;
-    }while(erreur>eps);
-    printf("La racine de f par la methode de newton est %.20lf \nLa methode a necessité %d\n",xn_1,compteur);
+        xn_1 = xn - ((xn - cos(xn)) / (1 + sin(xn)));
+        erreur = fabs((xn_1 - xn) / xn_1);
+        xn = xn_1;
+        compteur += 1;
+    } while (erreur > eps);
+    printf("La racine de f par la methode de newton est %.20lf \nLa methode a necessitï¿½ %d\n", xn_1, compteur);
 }
 
-void dichotomie(double eps,double a,double b)
+void dichotomie(double eps, double a, double b)
 {
     double c, erreur;
-    int compteur=0;
+    int compteur = 0;
     do
     {
-        c=(a+b)/2;
-        erreur=fabs(b-a);
-        if(fct(a)*fct(c)>0.0)
+        c = (a + b) / 2;
+        erreur = fabs(b - a);
+        if (fct(a) * fct(c) > 0.0)
         {
-            a=c;
+            a = c;
         }
-        else if(fct(a)*fct(c)<0.0)
+        else if (fct(a) * fct(c) < 0.0)
         {
-            b=c;
+            b = c;
         }
         else
         {
             printf("C'est la merde\n");
         }
-        compteur+=1;
-    }while(erreur>eps);
-    printf("La racine de f par la methode de newton est %.20lf \nLa methode a necessité %d\n",c,compteur);
+        compteur += 1;
+    } while (erreur > eps);
+    printf("La racine de f par la methode de newton est %.20lf \nLa methode a necessitï¿½ %d\n", c, compteur);
 }
 
-
-int main (int argc, char*argv[])
+int main(int argc, char *argv[])
 {
-    double x0=0.5,eps=pow(10,-12),a=0,b=1;  //ATTENTION x0 doit appartenir a [0,1]
-   // printf("Veuillez choisir l'erreur attendue\n");
-   // scanf("%f",eps);
-    pointfixe(x0,eps);
-    newton(x0,eps);
-    dichotomie(eps,a,b);
+    double x0 = 0.5, eps = pow(10, -12), a = 0.0, b = 1.0; // ATTENTION x0 doit appartenir a [0,1]
+    // printf("Veuillez choisir l'erreur attendue\n");
+    // scanf("%f",eps);
+    pointfixe(x0, eps);
+    newton(x0, eps);
+    dichotomie(eps, a, b);
     return 0;
 }
--- 3AS5/TP2/exo3.c
+++ 3AS5/TP2/exo3.c
@@ -3,7 +3,80 @@
 #include <math.h>
 #include <string.h>
 
+double f(double x)
+{
+    return (x - cos(x));
+}
+
+double fprime(double x)
+{
+    return (1 + sin(x));
+}
+
+void pointfixe(double x0, double epsilon)
+{
+    double xn1 = 0.0, xn = x0;
+    double erreur;
+    int compteur = 0;
+    do
+    {
+        xn1 = cos(xn);
+        erreur = fabs(xn1 - xn);
+        xn = xn1;
+        compteur += 1;
+    } while (erreur > epsilon);
+    printf("La solution par la méthode du point fixe est %lf et à été trouvée en %d itérations.\n", xn1, compteur);
+}
+
+void newton(double x0, double epsilon)
+{
+    double xn1 = 0.0, xn = x0;
+    double erreur;
+    int compteur = 0;
+    do
+    {
+        xn1 = xn - f(xn) / fprime(xn);
+        erreur = fabs(xn1 - xn);
+        xn = xn1;
+        compteur += 1;
+    } while (erreur > epsilon);
+    printf("La solution par la méthode de Newton est %lf et à été trouvée en %d itérations.\n", xn1, compteur);
+}
+
+void dichotomie(double a, double b, double epsilon)
+{
+    double c, erreur;
+    int compteur = 0;
+    do
+    {
+        c = (a + b) / 2;
+        erreur = fabs(b - a);
+        if (f(a) * f(c) > 0.0)
+        {
+            a = c;
+        }
+        else if (f(a) * f(c) < 0.0)
+        {
+            b = c;
+        }
+        else
+        {
+            printf("C'est la merde\n");
+        }
+        compteur += 1;
+    } while (erreur > epsilon);
+    printf("La solution par la méthode de Newton est %lf et à été trouvée en %d itérations.\n", c, compteur);
+}
+
 int main(int argc, char *argv[])
 {
+    double x0 = 0.5;
+    double a = 0.0, b = 1.0;
+    double epsilon = 1e-12;
+    double correction = 0.739085;
+    printf("La solution exacte est %lf.\n", correction);
+    pointfixe(x0, epsilon);
+    newton(x0, epsilon);
+    dichotomie(a, b, epsilon);
     return 0;
 }
