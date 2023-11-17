2924417ae6f3c4200e8345c97947c0da59c2fcbb
Mathis Gauthey <mathis.gauthey@gmail.com>
2023-11-17 12:54:57 +0100

    Working graphs

--- Java-Sorting-Algorithms/src/Graph.java
+++ Java-Sorting-Algorithms/src/Graph.java
@@ -1,60 +1,138 @@
-import java.util.Iterator;
-import java.util.LinkedList;
+
+import java.util.*;
 
 public class Graph {
 
-    // Number of vertices in the graph
-    private int V;
+    // Node class
+    public class Node {
+        // Attributes
+        int value; // A node has just a name
 
-    // array of linked lists (adj[]) where each element of the array represents a
-    // vertex, and the corresponding linked list represents the adjacent vertices of
-    // that vertex.
-    private LinkedList<Integer> adj[];
-    // IMPORTANT STUFF TO UNDERSTAND
-    // Array : int[] array = new int[size];
-    // List : List<Integer> list = new ArrayList<Integer>();
-    // Array of references to LinkedList<Integer> objects :
-    // private LinkedList<Integer>[] adj;
-    // Array of LinkedList<Integer> objects directly :
-    // private LinkedList<Integer> adj[];
+        // Constructor
+        public Node(int value) {
+            this.value = value;
+        }
+
+    }
+
+    // Attributes
+    int N; // Number of nodes in the graph
+    Map<Node, Set<Node>> adjList; // Adjacent dictionary of the graph
+    // The adjacent dictionary is a dictionary of nodes, where each node has a set
+    // of neighbor nodes
 
     // Constructor
-    @SuppressWarnings("unchecked")
-    Graph(int v) {
-        V = v;
-        adj = new LinkedList[v];
-        for (int i = 0; i < v; i++) {
-            adj[i] = new LinkedList<Integer>();
+    public Graph(int n) {
+        N = n;
+        this.adjList = new HashMap<>();
+        // We then create as many nodes as required
+        for (int i = 0; i < N; i++) {
+            addNode(i);
         }
     }
 
-    // Function to add an edge into the graph
-    void addEdge(int v, int w) {
-        // Add w to v's list.
-        adj[v].add(w);
+    // Create a node inside your graph if it does not exist
+    void addNode(int value) {
+        Node node = new Node(value);
+        adjList.putIfAbsent(node, new HashSet<>());
     }
 
-    void DFSUtil(int v, boolean visited[]) {
-        // Mark the current node as visited and print it
-        visited[v] = true;
-        System.out.println(v + " ");
+    // Find two nodes by their values and add an edge between them if they exist
+    void addEdge(int src, int dest) {
+        Node srcNode = findNode(src);
+        Node destNode = findNode(dest);
 
-        // Recur for all the vertices adjacent to this vertex
-        Iterator<Integer> i = adj[v].listIterator();
-        while (i.hasNext()) {
-            int n = i.next();
-            if (!visited[n])
-                DFSUtil(n, visited);
+        if (srcNode != null && destNode != null) {
+            adjList.get(srcNode).add(destNode);
+            adjList.get(destNode).add(srcNode);
         }
     }
 
-    void DFS(int v) {
-        // Mark all the vertices as false (not visited)
-        // In java, boolean array is initialized to false by default
-        boolean visited[] = new boolean[V];
+    // Find a node by its value in the graph list of nodes
+    // The java.util.HashMap.keySet() method in Java is used to create a set out of
+    // the key elements contained in the hash map.
+    Node findNode(int val) {
+        for (Node node : adjList.keySet()) { // For each node in the graph
+            if (node.value == val) {
+                return node;
+                // If we find the node in the set of nodes, we return it
+            }
+        }
+        return null; // Else we return null
+    }
 
-        // Call the recursive helper function to print DFS traversal
-        DFSUtil(v, visited);
+    // Print the whole graph via adjacent list
+    public void printGraph() {
+        // For each node in the graph...
+        for (Node node : adjList.keySet()) {
+            System.out.print(node.value + ": "); // Print the node value
+            // For each neighbor of the node...
+            for (Node neighbor : adjList.get(node)) {
+                System.out.print(neighbor.value + " "); // Print them
+            }
+            System.out.println(); // Print a new line
+        }
+    }
+
+    // DFS search starting at node with value start
+    void dfs(int start) {
+        Node startNode = findNode(start); // Find the node with value start
+        Set<Node> visited = new HashSet<>(); // Create a set of visited nodes
+        if (startNode != null) {
+            dfsRecursive(startNode, visited);
+        }
+    }
+
+    void dfsRecursive(Node currentNode, Set<Node> visited) {
+        // If the current node has not been visited yet...
+        if (!visited.contains(currentNode)) {
+            // Print the node value
+            System.out.print(currentNode.value + " ");
+            // And the node to the visited list
+            visited.add(currentNode);
+
+            // For each neighbor of the current node...
+            for (Node neighbor : adjList.get(currentNode)) {
+                // If the neighbor has not been visited yet...
+                dfsRecursive(neighbor, visited);
+            }
+        }
+    }
+
+    // BFS search starting at node with value start
+    void bfs(int start) {
+        Node startNode = findNode(start); // Find the node with value start
+        if (startNode != null) {
+            bfsTraversal(startNode);
+        }
+    }
+
+    void bfsTraversal(Node startNode) {
+        // Generate a queue (FIFO) and a visited list
+        Queue<Node> queue = new LinkedList<>();
+        Set<Node> visited = new HashSet<>();
+
+        // Add the start node to the queue and the visited list
+        queue.add(startNode);
+        visited.add(startNode);
+
+        // While the queue is not empty...
+        while (!queue.isEmpty()) {
+            // Get the first node in the queue
+            Node currentNode = queue.poll();
+            // Print it's value
+            System.out.print(currentNode.value + " ");
+
+            // For each neighbor of the current node...
+            for (Node neighbor : adjList.get(currentNode)) {
+                // If the neighbor has not been visited yet...
+                if (!visited.contains(neighbor)) {
+                    // Add it to the queue and the visited list
+                    queue.add(neighbor);
+                    visited.add(neighbor);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -66,13 +144,13 @@ public class Graph {
         g.addEdge(2, 4);
         g.addEdge(2, 5);
         g.addEdge(5, 6);
-        g.addEdge(6, 5);
 
-        System.out.println(
-                "Following is Depth First Traversal "
-                        + "(starting from vertex 2)");
+        g.printGraph();
 
-        // Function call
-        g.DFS(0);
+        System.out.println("\nDFS:");
+        g.dfs(0);
+        System.out.println("\nBFS:");
+        g.bfs(0);
     }
+
 }
